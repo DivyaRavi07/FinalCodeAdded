d67634b296b8ceac4a6ee309e9d38af9656651f3
DivyaRavi07 <divyaravi989@gmail.com>
2022-11-26 18:59:41 +0530

    Code Added in Github

---
+++ src/org/git/GitFinalWork.java
@@ -0,0 +1,14 @@
+package org.git;
+
+public class GitFinalWork {
+	private void systemModel() {
+		System.out.println("Model Name:HP");
+	}
+	
+	public static void main(String[] args) {
+		GitFinalWork n = new GitFinalWork();
+		n.systemModel();
+	}
+
+}
+
